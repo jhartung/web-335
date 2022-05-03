abc972ecd3145b3c11b9c0ce92f0a6d5ffa11db8
jhartung <joel.hartung@gmail.com>
2022-05-03 15:40:31 -0500

    Exercise 8.3

---
+++ week-8/hartung_calculator.py
@@ -0,0 +1,26 @@
+#
+# ============================================
+# Title: Exercise 8.3
+# Author: Professor Krasso
+# Date: 3 May 2022
+# Modified By: Joel Hartung
+# Description: hartung_calculator.py
+# Code Attribution: Code from the Exercise 8.3 document
+# ============================================
+
+# create an "add" function with two paramenters
+def add(a,b):
+    return (a+b)
+
+# create a "subtract" function with two parameters
+def subtract(a,b):
+    return (a-b)
+
+# create a "divide" function with two parameters
+def divide(a,b):
+    return (a/b)
+
+# call each function with a separate print statement
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
--- week-8/hartung_hello_world.py
+++ week-8/hartung_hello_world.py
@@ -1,1 +1,11 @@
+#
+# ============================================
+# Title: Exercise 8.2
+# Author: Professor Krasso
+# Date: 3 May 2022
+# Modified By: Joel Hartung
+# Description: hartung_hello_world.py
+# Code Attribution: Code from the Exercise 8.2 document
+# ============================================
+
 print('Hello World')
